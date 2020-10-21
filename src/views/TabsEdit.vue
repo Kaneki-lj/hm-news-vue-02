b4e82f94c27466f78288fb9d06727e68fafa3fbe
璃锦 <2373316055@qq.com>
2020-10-21 09:50:40 +0800

    栏目管理页

--- src/views/TabsEdit.vue
+++ src/views/TabsEdit.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
+    <!-- 头部 -->
     <hm-header>栏目管理</hm-header>
     <div class="tab-edit">
-      <!-- 头部 -->
       <!-- 删除 -->
       <div class="title">点击删除一下频道</div>
       <div class="list">
-        <div class="item" v-for="tab in activeTabs" :key="tab.id">
-          <div @click="del(tab.id)">{{ tab.name }}</div>
+        <div class="item" v-for="item in activeTabs" :key="item.id">
+          <div @click="del(item.id)">{{ item.name }}</div>
         </div>
       </div>
       <!-- 添加 -->
       <div class="title">点击添加一下频道</div>
       <div class="list">
-        <div class="item" v-for="tab in deactiveTabs" :key="tab.id">
-          <div @click="add(tab.id)">{{ tab.name }}</div>
+        <div class="item" v-for="item in deactiveTabs" :key="item.id">
+          <div @click="add(item.id)">{{ item.name }}</div>
         </div>
       </div>
     </div>
@@ -42,6 +42,7 @@ export default {
     async getactive() {
       // let activeTabs = JSON.parse(localStorage.getItem('activeTabs'))
       // let deactiveTabs = JSON.parse(localStorage.getItem('deactiveTabs'))
+
       // if (activeTabs && deactiveTabs) {
       //   this.activeTabs = activeTabs
       //   this.deactiveTabs = deactiveTabs
@@ -57,7 +58,7 @@ export default {
     // 删除
     del(id) {
       // 根据id找到对应的标题
-      let tab = this.activeTabs.find(v => v.id === id)
+      let tab = this.activeTabs.find(v => v.id == id)
       // 下面添加
       this.deactiveTabs.push(tab)
       // 上面删除
@@ -66,7 +67,7 @@ export default {
     // 添加
     add(id) {
       // 根据id找到对应栏目
-      let tab = this.activeTabs.find(v => v.id === id)
+      let tab = this.activeTabs.find(v => v.id == id)
       // 上面添加
       this.activeTabs.push(tab)
       // 下面删除
